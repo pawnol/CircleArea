755cd35f0db10d452912d1d415a336c3c39d414d
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-03-14 14:42:21 -0500

    Add files via upload

---
+++ CircleArea.cpp
@@ -0,0 +1,22 @@
+/*
+Circle Area
+3/14/2021
+The program should store the value 3.14159 in a constant called PI, the value
+4 in a variable called radius, and then calculates the area of a circle with
+the given radius. Currently, the program has several incomplete or missing
+lines, which are marked with comments. Using the comments as a guide, add the
+code to complete the program.
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    // declare a constant called PI with the value 3.14159
+    // declare a variable called radius with the value 4
+    double area = PI * pow(radius, 2);
+    cout << "Area of the circle: " << area;
+    return 0;
+}
